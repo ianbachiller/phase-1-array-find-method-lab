054c22f13c949e7506f17fc53c5d1be1fef895df
Ian Bachiller <ianbachise@gmail.com>
2024-02-05 20:31:31 -0500

    completes array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function superbowlWin(record) {
+    const winningYear = record.find((game) => game.result === "W");
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+      return undefined
+    }
+  }
+  
+  const winYear = superbowlWin(record);
+  console.log(winYear);
